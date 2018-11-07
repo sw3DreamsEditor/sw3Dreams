616565f24b047129ce5da88503468b227046f1b4
sw3DreamsEditor <44833097+sw3DreamsEditor@users.noreply.github.com>
2018-11-07 10:07:32 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <title>Sw3Dreams</title>
+        <meta charset="utf-8" />
+        <meta lang="de" />
+        <link href="design.css" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Inconsolata" href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+    </head>
+    <body>
+        <div id="header">
+            
+        </div>
+        <div id="nav">
+            
+        </div>
+        <div id="body">
+            <h1 id="title" class="title">Sw3Dreams (are made of this)</h1>
+            <p>Some random generic text which should either make people buy this shit or tell something about the company. idk really</p>
+            <p>test</p>
+        </div>
+        <div id="footer">
+            
+        </div>
+    </body>
+</html>
