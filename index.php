d1a6331135740a257aa7ca8ba0dd68dcd28f7ad8
sw3DreamsEditor2 <44833138+sw3DreamsEditor2@users.noreply.github.com>
2018-11-21 09:10:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         <div id="header">
             <div id="header2">
             <p>
-                <a class="headerlink" href="/about.php">&Uuml;ber uns</a><tab />|<tab /><a class="headerlink" href="/product.php">Produkt</a><tab />|<tab /><a class="headerlink" href="/index.php">Sw3Dreams</a>
+                <a class="headerlink" href="/about.php">&Uuml;ber uns</a><tab />|<tab /><a class="headerlink" href="/product.php">Produkt</a><tab />|<tab /><a class="headerlink" href="/index.php">Sw3Dreams</a><tab />|<tab />
                 <a class="headerlink" href="/Formular.php">LOL f&uuml;r Lars</a>
             </p>
             </div>
