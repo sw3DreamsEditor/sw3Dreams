1b75d3b90c95736e512fcfde6ea19144773b5f52
sw3DreamsEditor2 <44833138+sw3DreamsEditor2@users.noreply.github.com>
2018-11-21 09:06:35 +0100

    Fürs Formular neu ersteut
    <DOCTYPE html>
    <html>
        <head>
            <title>Sw3Dreams</title>
            <meta charset="utf-8" />
            <meta lang="de" />
            <link href="css/design.css" rel="stylesheet" />
            <link href="https://fonts.googleapis.com/css?family=Inconsolata" href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
        </head>
    <body>
        <div id="testheader">
                        <a class="headerlink" href="index.html">Home</a>
    
            </div>
        <div id="Formular">
                <input id="FormularName" name="FormularName" placeholder="Name">
                <input id="FormularPreis" name="FormularPreis" placeholder="Preis">
            </div>
        </body>
    </html>

---
+++ Formular.php
@@ -0,0 +1,20 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <title>Sw3Dreams</title>
+        <meta charset="utf-8" />
+        <meta lang="de" />
+        <link href="css/design.css" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Inconsolata" href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+    </head>
+<body>
+    <div id="testheader">
+                    <a class="headerlink" href="index.html">Home</a>
+
+        </div>
+    <div id="Formular">
+            <input id="FormularName" name="FormularName" placeholder="Name">
+            <input id="FormularPreis" name="FormularPreis" placeholder="Preis">
+        </div>
+    </body>
+</html>
