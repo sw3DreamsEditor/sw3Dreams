5f71f9692f182395158316646c9ad6d4c80c88ca
sw3DreamsEditor <44833097+sw3DreamsEditor@users.noreply.github.com>
2018-11-21 11:08:38 +0100

    Added insert

--- registration.php
+++ registration.php
@@ -5,9 +5,16 @@ $lastname=$_POST['lastname'];
 $pw=$_POST['password'];
 $cpw=$_POST['confirmpw'];
 
-// if username !exist && pw=cpw create user
-// else abort
+
+$existing = $mysqli->query("select $username from user");
+if (($existing!=$username) && ($pw==$cpw)) {
+    $pwhash = password_hash($pw, PASSWORD_DEFAULT);
+    $mysqli->query("insert into user (username, firstname, lastname, password) values ($username, $firstname, $lastname, $pwhash)");
+} else {
+    $userexists=true;
+}
 ?>
+
 <DOCTYPE html>
 <html>
     <head>
@@ -17,24 +24,18 @@ $cpw=$_POST['confirmpw'];
         <link href="design.css" rel="stylesheet" />
     </head>
     <body>
-        <div id="header">
-            
-        </div>
-        <div id="nav">
-            
-        </div>
-        <div id="body">
-            <form method="post">
-                <input name="username" type="text" placeholder="username" />
-                <input name="firstname" type="text" placeholder="firstname" />
-                <input name="lastname" type="text" placeholder="lastname" />
-                <input name="password" type="password" placeholder="password" />
-                <input name="confirmpw" type="password" placeholder="confirm password" />
-                <button type="submit" value="Registrieren">Registrieren</button>
-            </form>
-        </div>
-        <div id="footer">
-            
-        </div>
+        <?php
+        if ($userexists) {
+            echo "        <p>Der gew&auml;hlte Benutzername existiert bereits. Bitte w&auml;hle einen anderen.</p>"
+        }
+        ?>
+        <form method="post">
+            <input name="username" type="text" placeholder="username" />
+            <input name="firstname" type="text" placeholder="firstname" />
+            <input name="lastname" type="text" placeholder="lastname" />
+            <input name="password" type="password" placeholder="password" />
+            <input name="confirmpw" type="password" placeholder="confirm password" />
+            <button type="submit" value="Registrieren">Registrieren</button>
+        </form>
     </body>
 </html>
