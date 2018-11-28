af21966754039fe5316b446359ada8b5253ba4e0
sw3DreamsEditor <44833097+sw3DreamsEditor@users.noreply.github.com>
2018-11-28 08:15:26 +0100

    Update db.php

--- db.php
+++ db.php
@@ -7,4 +7,5 @@ $dbname = "sw3dreams";
 $mysqli = new mysqli($servername, $username, $password, $dbname);
 
 $mysqli->query("CREATE TABLE IF NOT EXISTS user (username varchar(255) primary key, firstname varchar(255), lastname varchar(255), password varchar(255))");
+echo $mysqli;
 ?>
