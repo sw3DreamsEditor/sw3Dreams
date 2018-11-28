8a49cce3a1c1368b458f20ffa4130a51f9b87bfd
sw3DreamsEditor <44833097+sw3DreamsEditor@users.noreply.github.com>
2018-11-28 10:04:56 +0100

    Update db.php

--- db.php
+++ db.php
@@ -6,16 +6,6 @@ $dbname = "sw3dreams";
 
 $mysqli = new mysqli($servername, $username, $password, $dbname);
 
-$out = $mysqli->query("CREATE TABLE IF NOT EXISTS user (username varchar(255) primary key, firstname varchar(255), lastname varchar(255), password varchar(255))");
-echo $out;
-$out = $mysqli->query("SELECT * FROM user");
-
-if ($out->num_rows > 0) {
-    // output data of each row
-    while($row = $out->fetch_assoc()) {
-        echo "firstname: " . $row["firstname"]. " - lastname: " . $row["lastname"]. " - username: " . $row["username"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
+$mysqli->query("CREATE TABLE IF NOT EXISTS user (username varchar(255) primary key, firstname varchar(255), lastname varchar(255), password varchar(255))");
+$mysqli->query("CREATE TABLE IF NOT EXISTS product (productname varchar(255) primary key, price float, desc varchar(255), picpath varchar(255))");
 ?>
