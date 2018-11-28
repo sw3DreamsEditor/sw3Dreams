16297f80cdadc17e42f38b496c360ebcfae71136
sw3DreamsEditor <44833097+sw3DreamsEditor@users.noreply.github.com>
2018-11-28 08:20:31 +0100

    Update db.php

--- db.php
+++ db.php
@@ -8,4 +8,14 @@ $mysqli = new mysqli($servername, $username, $password, $dbname);
 
 $out = $mysqli->query("CREATE TABLE IF NOT EXISTS user (username varchar(255) primary key, firstname varchar(255), lastname varchar(255), password varchar(255))");
 echo $out;
+$out = $mysqli->query("SELECT * FROM user");
+
+if ($out->num_rows > 0) {
+    // output data of each row
+    while($row = $out->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
 ?>
