3030cfb30bd57edf6fcacd787c0603eee3b46a76
sw3DreamsEditor <44833097+sw3DreamsEditor@users.noreply.github.com>
2018-11-28 10:00:29 +0100

    added enctype so picture can be accessed by php
    also added the WHOLE FUCKIN PHP TO FUCKIN DO THIS SHIT but no major things

--- Formular.php
+++ Formular.php
@@ -9,16 +9,41 @@
     </head>
 <body>
     <div id="testheader">
-                    <a class="headerlink" href="index.php">Home</a>
+        <a class="headerlink" href="index.php">Home</a>
+    </div>
+    <div id="Formular">
+        <?php
+            $servername = "localhost";
+            $username = "sw3dreams";
+            $password = "sml12345";
+            $dbname = "sw3dreams";
 
-        </div>
-      <div id="Formular">
-        <form id="Form"method="post">
-            <input id="FormularName" name="FormularName" placeholder="Name"/>
-            <input id="FormularPreis" name="FormularPreis" placeholder="Preis"/>
-            <textarea id="FormularBeschrieb" name="FormularBeschrieb" placeholder="Beschreiben Sie das Produkt"></textarea>
-            <input name="FormularDatei" type="file" accept="image/jpeg">
-            <button type="submit" value="Submit">Submit</button>
+            $mysqli = new mysqli($servername, $username, $password, $dbname);
+
+            $uploads_dir = '/var7www/sw3dreams/pruducts/pictures';
+            $picdir = '';
+            foreach ($_FILES["pictures"]["error"] as $key => $error) {
+                if ($error == UPLOAD_ERR_OK) {
+                    $tmp_name = $_FILES["pictures"]["tmp_name"][$key];
+                    // basename() may prevent filesystem traversal attacks;
+                    // further validation/sanitation of the filename may be appropriate
+                    $name = basename($_FILES["pictures"]["name"][$key]);
+                    $picdir = "$uploads_dir/$name";
+                    move_uploaded_file($tmp_name, "$picdir");
+                } else {
+                    echo "<h1>Whoops, there went something wrong with the upload of the picture</h1>";
+                    echo "\n<p>aka you (client-side) fucked up</p>";
+                }
+            }
+            $mysqli->query("insert into product (productname, price, desc, picpath) values ($_POST['productname'], $_POST['price'], $_POST['desc'], $picdir)");
+        
+            echo "            <form id="Form" enctype="multipart/form-data" method="post">";
+            echo "            <input id="FormularName" name="productname" placeholder="Name" />";
+            echo "            <input id="FormularPreis" name="price" placeholder="Preis" />";
+            echo "            <textarea id="FormularBeschrieb" name="desc" placeholder="Beschreiben Sie das Produkt"></textarea>";
+            echo "            <input type="hidden" name="MAX_FILE_SIZE" value="30000" />";
+            echo "            <input name="picture" type="file" accept="image/jpeg" />";
+            echo "            <button type="submit" value="Submit">Submit</button>";
         </form>
       </div>
     </body>
