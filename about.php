621e95bbb77cdc109cf97d3bd555a14740790fde
sw3DreamsEditor2 <44833138+sw3DreamsEditor2@users.noreply.github.com>
2018-11-28 08:24:51 +0100

    Create about.php

---
+++ about.php
@@ -0,0 +1,12 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <title>Sw3Dreams</title>
+        <meta charset="utf-8" />
+        <meta lang="de" />
+        <link href="design.css" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Inconsolata" href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+  </head>
+  
+  
+  </html>
