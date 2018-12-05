f3f761a836cc7dd934cd1c237641ba5e03b590d8
sw3DreamsEditor <44833097+sw3DreamsEditor@users.noreply.github.com>
2018-12-05 10:02:02 +0100

    Update registration.php

--- registration.php
+++ registration.php
@@ -22,7 +22,7 @@ $result = $mysqli->query($query);
 
 /* fetch object array */
 while ($obj = $result->fetch_object()) {
-    if (!(in_array($username, $obj->username) && ($pw==$cpw)) {
+    if (!(in_array($username, $obj->username)) && ($pw==$cpw)) {
         $pwhash = password_hash($pw, PASSWORD_DEFAULT);
         $insertstatement = "insert into user (username, firstname, lastname, pwd) values ($username, $firstname, $lastname, $pwhash)";
         $whaduhec = $mysqli->query($insertstatement);
