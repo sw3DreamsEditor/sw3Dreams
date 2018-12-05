f848882d2367c8823bb175ed9b60064684ba7272
sw3DreamsEditor2 <44833138+sw3DreamsEditor2@users.noreply.github.com>
2018-12-05 08:41:17 +0100

    lol yeeet
    <body>
        <h1 id="Shoptitle">Willkommen in unserem Shop</h1>
    
        <div class="Productdivs">
            <img class="ProductPics" src="https://i.ytimg.com/vi/0yZxkDm9wdE/hqdefault.jpg">
            <p>M'kaaaaaay</p>
            <p>2000 CHF</p>
            <p>Bild gemacht von Lars <br>Aeberhardt</p>
        </div>
        <div class="Productdivs">
            <img class="ProductPics" src="https://i.kym-cdn.com/entries/icons/original/000/026/913/excuse.jpg">
            <p>Excuse me WTF</p>
            <p>666 CHF</p>
            <p>Bild gemacht von Lars <br>Aeberhardt</p>
        </div>
        <div class="Productdivs">
            <img class="ProductPics" src="https://hamburg.mitvergnuegen.com/wp-content/uploads/sites/2/2018/01/maxresdefault-680x383.jpg">
            <p>No God</p>
            <p>10 CHF</p>
            <p>Bild gemacht von Lars <br>Aeberhardt</p>
        </div>
        <div class="Productdivs">
            <img class="ProductPics" src="https://pbs.twimg.com/profile_images/817634157006229504/vEsZewpd_400x400.jpg">
            <p>Win?</p>
            <p>50 CHF</p>
            <p>Bild gemacht von Lars <br>Aeberhardt</p>
        </div>
        <div class="Productdivs">
            <img class="ProductPics" src="https://cdn.someecards.com/posts/memes-about-work-3cD.jpg">
            <p>The Face</p>
            <p>100 CHF</p>
            <p>Bild gemacht von Lars <br>Aeberhardt</p>
        </div>
        <div class="Productdivs">
            <img class="ProductPics" src="https://www.todaysparent.com/wp-content/uploads/2017/06/when-your-kid-becomes-a-meme-1024x576-1497986561.jpg">
            <p>When u Think</p>
            <p>30 CHF</p>
            <p>Bild gemacht von Lars <br>Aeberhardt</p>
        </div>
        <div class="Productdivs">
            <img class="ProductPics" src="https://assets.entrepreneur.com/content/3x2/2000/20180703190744-rollsafe-meme.jpeg?width=700&crop=2:1">
            <p>Genius</p>
            <p>90 CHF</p>
            <p>Bild gemacht von Lars <br>Aeberhardt</p>
        </div>
        <div class="Productdivs">
            <img class="ProductPics" src="https://s17346.pcdn.co/wp-content/uploads/2017/10/night-shift-memes.jpg">
            <p>Night Shifts</p>
            <p>58.99 CHF</p>
            <p>Bild gemacht von Lars <br>Aeberhardt</p>
        </div>
    </body>

--- product.php
+++ product.php
@@ -8,6 +8,58 @@
         <link href="design.css" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Inconsolata" href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
   </head>
-  
+  <body>
+    <h1 id="Shoptitle">Willkommen in unserem Shop</h1>
+
+    <div class="Productdivs">
+        <img class="ProductPics" src="https://i.ytimg.com/vi/0yZxkDm9wdE/hqdefault.jpg">
+        <p>M'kaaaaaay</p>
+        <p>2000 CHF</p>
+        <p>Bild gemacht von Lars <br>Aeberhardt</p>
+    </div>
+    <div class="Productdivs">
+        <img class="ProductPics" src="https://i.kym-cdn.com/entries/icons/original/000/026/913/excuse.jpg">
+        <p>Excuse me WTF</p>
+        <p>666 CHF</p>
+        <p>Bild gemacht von Lars <br>Aeberhardt</p>
+    </div>
+    <div class="Productdivs">
+        <img class="ProductPics" src="https://hamburg.mitvergnuegen.com/wp-content/uploads/sites/2/2018/01/maxresdefault-680x383.jpg">
+        <p>No God</p>
+        <p>10 CHF</p>
+        <p>Bild gemacht von Lars <br>Aeberhardt</p>
+    </div>
+    <div class="Productdivs">
+        <img class="ProductPics" src="https://pbs.twimg.com/profile_images/817634157006229504/vEsZewpd_400x400.jpg">
+        <p>Win?</p>
+        <p>50 CHF</p>
+        <p>Bild gemacht von Lars <br>Aeberhardt</p>
+    </div>
+    <div class="Productdivs">
+        <img class="ProductPics" src="https://cdn.someecards.com/posts/memes-about-work-3cD.jpg">
+        <p>The Face</p>
+        <p>100 CHF</p>
+        <p>Bild gemacht von Lars <br>Aeberhardt</p>
+    </div>
+    <div class="Productdivs">
+        <img class="ProductPics" src="https://www.todaysparent.com/wp-content/uploads/2017/06/when-your-kid-becomes-a-meme-1024x576-1497986561.jpg">
+        <p>When u Think</p>
+        <p>30 CHF</p>
+        <p>Bild gemacht von Lars <br>Aeberhardt</p>
+    </div>
+    <div class="Productdivs">
+        <img class="ProductPics" src="https://assets.entrepreneur.com/content/3x2/2000/20180703190744-rollsafe-meme.jpeg?width=700&crop=2:1">
+        <p>Genius</p>
+        <p>90 CHF</p>
+        <p>Bild gemacht von Lars <br>Aeberhardt</p>
+    </div>
+    <div class="Productdivs">
+        <img class="ProductPics" src="https://s17346.pcdn.co/wp-content/uploads/2017/10/night-shift-memes.jpg">
+        <p>Night Shifts</p>
+        <p>58.99 CHF</p>
+        <p>Bild gemacht von Lars <br>Aeberhardt</p>
+    </div>
+</body>
+
   
   </html>
