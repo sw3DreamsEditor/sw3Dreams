d8491106d1c3d7a749c6714a64cf65b80fac3de3
sw3DreamsEditor2 <44833138+sw3DreamsEditor2@users.noreply.github.com>
2018-12-05 08:45:35 +0100

    Update about.php

--- about.php
+++ about.php
@@ -7,6 +7,16 @@
         <link href="design.css" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Inconsolata" href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
   </head>
+  <body>
+    
+            <div id="header">
+            <div id="header2">
+            <p>
+                <a class="headerlink" href="/about.php">&Uuml;ber uns</a><tab />|<tab /><a class="headerlink" href="/product.php">Produkt</a><tab />|<tab /><a class="headerlink" href="/index.php">Sw3Dreams</a><tab />|<tab />
+                <a class="headerlink" href="/Formular.php">LOL f&uuml;r Lars</a>
+            </p>
+            </div>
+        </div>
   
-  
+    </body> 
   </html>
