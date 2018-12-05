8b3161ca678319ba827d836e6021a940f87dbd4c
sw3DreamsEditor2 <44833138+sw3DreamsEditor2@users.noreply.github.com>
2018-12-05 08:45:00 +0100

    header
            <div id="header">
                <div id="header2">
                <p>
                    <a class="headerlink" href="/about.php">&Uuml;ber uns</a><tab />|<tab /><a class="headerlink" href="/product.php">Produkt</a><tab />|<tab /><a class="headerlink" href="/index.php">Sw3Dreams</a><tab />|<tab />
                    <a class="headerlink" href="/Formular.php">LOL f&uuml;r Lars</a>
                </p>
                </div>
            </div>

--- Formular.php
+++ Formular.php
@@ -8,9 +8,14 @@
         <link href="https://fonts.googleapis.com/css?family=Inconsolata" href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
     </head>
 <body>
-    <div id="testheader">
-        <a class="headerlink" href="index.php">Home</a>
-    </div>
+           <div id="header">
+            <div id="header2">
+            <p>
+                <a class="headerlink" href="/about.php">&Uuml;ber uns</a><tab />|<tab /><a class="headerlink" href="/product.php">Produkt</a><tab />|<tab /><a class="headerlink" href="/index.php">Sw3Dreams</a><tab />|<tab />
+                <a class="headerlink" href="/Formular.php">LOL f&uuml;r Lars</a>
+            </p>
+            </div>
+        </div>
     <div id="Formular">
         <?php
             $servername = "localhost";
